d4780f76bb9c71e310191fbff36ec88508a92fcc
putama <prasetya_utama@brown.edu>
2017-12-03 19:22:26 -0500

    update

--- temp-consistent-GAN/translate.py
+++ temp-consistent-GAN/translate.py
@@ -1,33 +1,33 @@
-import os
-import torch
 from options.test_options import TestOptions
 from data.data_loader import CreateDataLoader
 from models.models import create_model
 from util.util import save_image
+from util.util import mkdirreplace
 
 opt = TestOptions().parse()
 opt.nThreads = 1   # test code only supports nThreads = 1
-opt.batchSize = 2  # test code only supports batchSize = 1
+opt.batchSize = 1  # test code only supports batchSize = 1
 opt.serial_batches = True  # no shuffle
 opt.no_flip = True  # no flip
 AtoB = opt.which_direction == 'AtoB'
 
 data_loader = CreateDataLoader(opt)
 dataset = data_loader.load_data()
-# model = create_model(opt)
+model = create_model(opt)
 
+rootpath = "imgs/results/"
+mkdirreplace(rootpath)
+historydict = {}
 for i, data in enumerate(dataset):
-    if i == 0:
-        prevframe = data['A' if AtoB else 'B'][1:2]
+    if AtoB:
+        if historydict.has_key(data['A_paths'][0]):
+            break
+        filepath = rootpath + "fake_B_{}.jpg".format(str(i))
+        im_fake = model.translateA(data['A'])
+        historydict[data['A_paths'][0]] = 1
     else:
-        nextframe = data['A' if AtoB else 'B'][0:1]
-        data['A' if AtoB else 'B'] = torch.cat((prevframe, nextframe), 0)
-
-        # translate the data using trained model here
-        
-
-        # update prevframe from this batch
-        prevframe = data['A' if AtoB else 'B'][1:2]
-    print 'done'
-    # model.set_input(data)
-    # model.translate()
+        if historydict.has_key(data['B_paths'][0]):
+            break
+        filepath = rootpath + "fake_A_{}.jpg".format(str(i))
+        im_fake = model.translateB(data['B'][0])
+    save_image(im_fake, filepath)
