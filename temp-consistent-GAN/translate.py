42c8aae80adb69f183402d517f9ba14051c5b49b
putama <prasetya_utama@brown.edu>
2017-12-17 22:07:32 -0500

    translate fix

--- temp-consistent-GAN/translate.py
+++ temp-consistent-GAN/translate.py
@@ -6,39 +6,37 @@ from util.util import tensor2im
 from util.util import mkdirreplace
 import cv2
 
-rootpath = "imgs/results/"
-mkdirreplace(rootpath)
-
-frame_width = 256
-frame_height = 256
-out = cv2.VideoWriter('outpylatest.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 20, (frame_width,frame_height))
-
 opt = TestOptions().parse()
 opt.nThreads = 1   # test code only supports nThreads = 1
 opt.batchSize = 1  # test code only supports batchSize = 1
 opt.serial_batches = True  # no shuffle
 opt.no_flip = True  # no flip
+opt.resize_or_crop = "scale_width" # no random crop
+opt.dataset_mode = "unaligned" # unaligned dataset
+opt.phase = "test"
+opt.model = "cycle_gan"
+opt.no_dropout = True
 AtoB = opt.which_direction == 'AtoB'
 
 data_loader = CreateDataLoader(opt, "test")
 dataset = data_loader.load_data()
 model = create_model(opt)
 
+frame_width = 256
+frame_height = 256
+out = cv2.VideoWriter(opt.name+'.avi', cv2.VideoWriter_fourcc('M','J','P','G'), 20, (frame_width,frame_height))
+
 historydict = {}
 for i, data in enumerate(dataset):
     if AtoB:
         if historydict.has_key(data['A_paths'][0]):
             break
-        filepath = rootpath + "fake_B_{}.jpg".format(str(i))
         im_fake = model.translateA(data['A'])
         historydict[data['A_paths'][0]] = 1
     else:
         if historydict.has_key(data['B_paths'][0]):
             break
-        filepath = rootpath + "fake_A_{}.jpg".format(str(i))
         im_fake = model.translateB(data['B'][0])
         historydict[data['B_paths'][0]] = 1
-    save_image(im_fake, filepath)
-    print "saved to " + filepath
     out.write(im_fake)
 out.release()
